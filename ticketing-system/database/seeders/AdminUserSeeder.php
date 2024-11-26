d9aa8e3806884434b25aa126ba8ab3981eb9989d
Simon <simon@equadtech.com>
2024-11-26 22:15:12 +0800

    seeder modification

--- ticketing-system/database/seeders/AdminUserSeeder.php
+++ ticketing-system/database/seeders/AdminUserSeeder.php
@@ -19,5 +19,23 @@ class AdminUserSeeder extends Seeder
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
         ]);
+
+        User::create([
+            'name' => 'Liong Sooook Yik',
+            'email' => 'liongsy020601@gmail.com',
+            'password' => Hash::make('qweqweqwe'),
+            'uuid' => Str::uuid(),
+            'phone' => '0123456789',
+            'is_admin' => false,
+        ]);
+
+        User::create([
+            'name' => 'Simon Chock',
+            'email' => 'user@wanderworks.my',
+            'password' => Hash::make('qweqweqwe'),
+            'uuid' => Str::uuid(),
+            'phone' => '01234567892',
+            'is_admin' => false,
+        ]);
     }
 }
