f9ae386bcdac1693cb0edf55ee0e7b6da02630c8
MunchMate <munchmate888@outlook.com>
2024-11-27 01:02:06 +0800

    change email position

--- ticketing-system/app/Http/Controllers/StripeWebhookController.php
+++ ticketing-system/app/Http/Controllers/StripeWebhookController.php
@@ -32,12 +32,12 @@ class StripeWebhookController extends Controller
     private function handleCheckoutSessionCompleted($payload)
     {
         DB::beginTransaction();
-     \Log::info('Checkout session completed');
+        \Log::info('Checkout session completed');
         try {
             $session = $payload['data']['object'];
             $metadata = $session['metadata'];
             $productId = $metadata['product_id'];
-    
+
             // Check if package exists
             $package = Package::where('stripe_package_id', $productId)->first();
             if (!$package) {
@@ -45,7 +45,7 @@ class StripeWebhookController extends Controller
                 throw new \Exception('Package not found');
             }
             Log::info('Package found', context: $package->toArray());
-    
+
             // Find existing user or create new one
             $user = User::firstOrCreate(
                 ['email' => $session['customer_details']['email']],
@@ -55,37 +55,37 @@ class StripeWebhookController extends Controller
                     'uuid' => Str::uuid()
                 ]
             );
-    
+
             Log::info('User found or created', context: $user->toArray());
-    
+
             // Create ticket
             $ticket = Ticket::create([
                 'user_uuid' => $user->uuid,
                 'package_id' => $package->id,
                 'status' => TicketStatus::Active,
-                'is_unlimited' => (int)($package->pass_type === 0),
+                'is_unlimited' => (int) ($package->pass_type === 0),
                 'available_pass' => $package->pass_type ?: 0,
                 'valid_until' => now()->addDays(30),
                 'ending_notification_sent' => false,
             ]);
 
-            if($ticket){
+
+            Log::info(message: 'Ticket created', context: $ticket->toArray());
+
+            DB::commit();
+            if ($ticket) {
                 Mail::to($user->email)->send(new TicketCreated($ticket, $user));
             }
-    
-            Log::info(message: 'Ticket created', context: $ticket->toArray());
-    
-            DB::commit();
             return response()->json(['status' => 'success']);
-    
+
         } catch (\Exception $e) {
             DB::rollBack();
-    
+
             Log::error('Webhook processing failed', [
                 'error' => $e->getMessage(),
                 'trace' => $e->getTraceAsString()
             ]);
-    
+
             return response()->json([
                 'status' => 'error',
                 'message' => 'Failed to process webhook'
